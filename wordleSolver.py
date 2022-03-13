aca63461fcb1203b0818f655add77b601dd0e0c7
Otavio de Meira Lima <otaviomeira2008@hotmail.com>
2022-03-12 23:56:49 -0300

    Wrong letter guess working

--- wordleSolver.py
+++ wordleSolver.py
@@ -8,7 +8,7 @@ for word in word_list:
 
 print(len(five_letter_word_list))
 
-five_letter_word_list = ['andre', 'otavi', 'aaaaa', 'abcde', 'abcdb', 'otbvi']
+# five_letter_word_list = ['andre', 'otovi', 'aaaaa', 'abcde', 'abcdb', 'otbvi']
 
 # print("0 is for nonexistent\n1 is for correct letter, but in the wrong place\n2 is for correct letter at the correct place")
 guess = input("Enter your guess: ")
@@ -29,29 +29,35 @@ for i in range(5):
     elif (status_letters[i] == '2'):
         correct_letters.append((guess[i], i))
 
-for w in five_letter_word_list:
+it = 0
+
+while it < len(five_letter_word_list):
+
+    current_word = five_letter_word_list[it].lower()
 
     #if the word contains the nonexistent letter, remove from list
     for nl in nonexistent_letters:
-        print("Testing word", w, "with letter ", nl)
-        if (nl in w):
-            five_letter_word_list.remove(w)
-            print(w, " contains ", nl, ". Removed from list.")
+        if (nl in current_word):
+            five_letter_word_list.pop(it)
+            it -= 1
             leave = True
 
-    # #if the word does not contains existent letter or contains it in the wrong place, remove from list
-    # if (not leave):
-    #     for scl in semi_correct_letters:
-    #         if (not (scl[0] in word)):
-    #             five_letter_word_list.remove(word)
-    #             leave = True
-    #             break
 
-    #         else:
-    #             if (word[scl[1]] == scl[0]):
-    #                 five_letter_word_list.remove(word)
-    #                 leave = True
-    #                 break
+    #if the word does not contains existent letter or contains it in the wrong place, remove from list
+    if (not leave):
+        for scl in semi_correct_letters:
+            if (not (scl[0] in current_word)):
+                five_letter_word_list.pop(it)
+                it -= 1
+                leave = True
+            else:
+                if (current_word[scl[1]] == scl[0]):
+                    five_letter_word_list.pop(it)
+                    it -= 1
+                    leave = True
+
+    it += 1
+
 
     # #if the word does not contains the correct letter at a certain place, remove from list
     # if (not leave):
